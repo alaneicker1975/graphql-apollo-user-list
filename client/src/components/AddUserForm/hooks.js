8662829f9bb8feb3f9b63060be5fab7c52af09d8
Alan Eicker <alaneicker@gmail.com>
2021-03-15 07:53:50 -0500

    Update hooks.js

--- client/src/components/AddUserForm/hooks.js
+++ client/src/components/AddUserForm/hooks.js
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from './mutations';
 import { GET_USERS } from '../Users/queries';
+import { useAppContext } from '../../context/AppContext';
 
 export const useAddUser = () => {
+  const { currentPage, perPageLimit } = useAppContext();
+
   const [userData, setUserData] = useState({
     first_name: '',
     last_name: '',
@@ -13,9 +16,16 @@ export const useAddUser = () => {
 
   const [addUser] = useMutation(ADD_USER, {
     update: (cache, { data: { user } }) => {
-      const { users } = cache.readQuery({ query: GET_USERS });
-      users.data = [...users.data, user];
-      cache.writeQuery({ query: GET_USERS }, users);
+      const query = GET_USERS;
+      const variables = { page: currentPage, limit: perPageLimit };
+
+      const data = cache.readQuery({ query, variables });
+
+      const newData = {
+        users: { ...data.users, data: [...data.users.data, user.data] },
+      };
+
+      cache.writeQuery({ query, variables, data: newData });
     },
   });
 
